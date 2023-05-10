24b6cfb560ecf126ff1389011edbbe114de612e5
Ivan Dutra Goetze <133060521+ivancpdi@users.noreply.github.com>
2023-05-09 23:09:23 -0300

    Primeiro commit
    Aula 1 
    Dia 09/05/2023

---
+++ funcoesAritimetica/main.lua
@@ -0,0 +1,46 @@
+-----------------------------------------------------------------------------------------
+--
+-- main.lua
+--
+-----------------------------------------------------------------------------------------
+
+-- Your code here
+
+-- Variavel global. (deve ser evitado)
+    -- Funções crianda em outro script.
+    vidas = 2
+    print(vidas)
+    print("O valor de vidas:" .. vidas)
+-- 
+-- Variavel local. (so lida dentro do script) 
+    -- Para declarar a variavel local deve se ter a -- definição local
+    local pontos = 10
+    print(pontos)
+    print("O valor de pontos:" .. pontos)
+--
+-- Operações aritiméticas
+    -- Soma
+    local laranjas = 10
+
+    laranjas = laranjas + 5
+    print(laranjas)
+
+    local bananas = 5
+
+    -- Soma entre variaveis
+    local cesta = 0
+    cesta = laranjas + bananas
+    print ("A soma entre as variaveis laranjas e bananas é: " .. cesta)
+
+    -- Subtração
+    cesta = laranjas - bananas
+    bananas = bananas - 1
+
+    -- Multiplicação
+    local carros = 8
+    carros = carros * 2
+    print("Quantidade de carros: " .. carros)
+
+    carros = carros * 0.5
+    print(carros)
+
